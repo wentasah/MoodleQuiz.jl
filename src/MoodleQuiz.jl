cee44335c20ffd9ff3ef56e1def85c1197506c4e
Michal Sojka <michal.sojka@cvut.cz>
2021-01-21 02:34:20 +0100

    Add support for multiple question types, including cloze

--- src/MoodleQuiz.jl
+++ src/MoodleQuiz.jl
@@ -5,6 +5,7 @@ using FileIO
 using LightXML
 using HttpCommon
 using Markdown
+using OrderedCollections
 using Base: @kwdef
 using Pipe: @pipe
 using Base64
@@ -13,16 +14,98 @@ import Base.show
 
 export Question, Category, Quiz, moodle_xml, @cz_str, @en_str
 
+export TrueFalse, True, False, YesNo, Yes, No, MultiChoice, SingleChoice, Choice
+export Correct, Wrong, Essay, ShortAnswer, Numerical
+
+## Helper functions
+ensurearray(x) = if isa(x, Array); x else [ x ] end
+
+function escape_chars(io::IO, s::AbstractString, chars)
+    a = Iterators.Stateful(s)
+    for c::AbstractChar in a
+        if c in chars
+            print(io, '\\', c)
+        else
+            print(io, c)
+        end
+    end
+end
+
+## Data types & contructors
 @kwdef struct Text
     text::Markdown.MD
     lang::String
 end
+macro cz_str(text) return :( Text(text=Markdown.parse($text), lang="cs") ) end
+macro en_str(text) return :( Text(text=Markdown.parse($text), lang="en") ) end
+
+# Answer data types
+
+abstract type Answer end
+
+struct Essay <: Answer end
+
+struct TrueFalse <: Answer
+    correct::Bool
+end
+
+True() = TrueFalse(true)
+False() = TrueFalse(false)
+
+struct YesNo <: Answer
+    yes::Bool
+end
+
+Yes() = YesNo(true)
+No() = YesNo(false)
+
+struct Choice
+    correct::Bool
+    content::Vector{Any}
+end
+
+Choice(content; correct::Bool) = Choice(correct, ensurearray(content))
+
+Correct(content) = Choice(content, correct=true)
+Wrong(content) = Choice(content, correct=false)
+
+@enum AnswerNumbering none=0 abc=1 ABCD=2 numeric=3
+
+@kwdef struct MultiChoice <: Answer
+    single::Bool = false
+    shuffle::Bool = true
+    correct_feedback = ""
+    partiallycorrectfeedback = ""
+    incorrectfeedback = ""
+    answernumbering::AnswerNumbering = none
+
+    choices::Vector{Choice}
+    # TODO: Enforce single correct choice if single==true
+end
+
+MultiChoice(choices; kwargs...) = MultiChoice(; choices, kwargs...)
+SingleChoice(choices; kwargs...) = MultiChoice(; choices, single=true, kwargs...)
+
+struct Numerical <: Answer
+    val::Real
+    tolerance::Real
+end
+Numerical(val) = Numerical(val, 0.0)
+
+struct ShortAnswer <: Answer
+    text::String
+    fraction::Int               # 0 - 100 (or points for cloze)
+end
+ShortAnswer(text) = ShortAnswer(text, 100)
+
+# Question, Category, Quiz
 
 @kwdef struct Question
     id::String
     points::Float64
     dates::String
     content::Vector{Any}
+    answer::Union{Answer, Nothing} = nothing
 
     responseformat::String = "editorfilepicker"
     responserequired::Bool = true
@@ -33,16 +116,20 @@ end
 
 struct Category
     name::String
-    questions::Vector{Question}
+    questions::OrderedDict{String,Question}
 end
+Category(name) = Category(name, OrderedDict())
+default_category = Category("")
 
-Category(name) = Category(name, [])
+Question(id, points, dates, content, answer=nothing; kwargs...) =
+    Question(default_category, id, points, dates, content, answer, kwargs...)
 
-Question(id, points, dates, content; kwargs...) = Question(; id=id, points=points, dates=dates, content=content, kwargs...)
-
-function Question(category::Category, id, points, dates, content; kwargs...)
-    q = Question(; id=id, points=points, dates=dates, content=content, kwargs...)
-    push!(category.questions, q)
+function Question(category::Category, id, points, dates, content, answer=nothing; kwargs...)
+    if isnothing(answer) && !any(typeof.(content) .<: Answer)
+        answer = Essay()
+    end
+    q = Question(; id, points, dates, content, answer, kwargs...)
+    category.questions[q.id] = q
     return q
 end
 
@@ -54,12 +141,67 @@ end
 Quiz(q::Question) = Quiz(questions=[q])
 Quiz(c::Category) = Quiz(categories=[c])
 
+## Preview methods
+
+preview(io::IO, ::Nothing) = nothing
+preview(io::IO, ::Essay) = print(io, "Essay")
+preview(io::IO, ::ShortAnswer) = print(io, "ShortAnswer")
+preview(io::IO, tf::TrueFalse) = print(io, "($(tf.correct ? "x" : " ")) True  ($(tf.correct ? " " : "x")) False")
+preview(io::IO, yn::YesNo) = print(io, "($(yn.yes ? "x" : " ")) Yes  ($(yn.yes ? " " : "x")) No")
+function preview(io::IO, choice::Choice)
+    for val in choice.content
+        print(io, repr("text/html", val))
+    end
+end
+
+
+function preview(io::IO, mc::MultiChoice)
+    print(io, "<ul>")
+    for choice in mc.choices
+        x = choice.correct ? "x" : " "
+        print(io, "<li>" * (mc.single ? "($x)" : "[$x]") * " ")
+        preview(io, choice)
+        print(io, "</li>")
+    end
+    println(io, "</ul>")
+end
+
+function preview(io::IO, q::Question)
+    println(io, "<h3>Question $(q.id)</h3>")
+    println(io, "<div class=points>Points: $(q.points)</div>")
+    println(io, to_html(q))
+    println(io, "<div class=answer>Answer: ")
+    preview(io, q.answer)
+    println(io, "</div>")
+end
+
+function preview(io::IO, cat::Category)
+    println(io, "<h2>Category $(cat.name)</h2>")
+    for q in cat.questions
+        preview(io, q)
+    end
+end
+
+function preview(io::IO, quiz::Quiz)
+    println(io, "<h1>Quiz</h1>")
+    for q in quiz.questions
+        preview(io, q)
+    end
+    for cat in quiz.categories
+        preview(io, cat)
+    end
+end
+
+function preview(object)
+    open("questions.html", "w") do io
+        preview(io, object)
+    end
+    run(`firefox-reload`)
+end
+
 function Base.show(io::IO, ::MIME"text/html", t::Text)
     print(io, """<span lang="$(t.lang)" class="multilang">""" * repr("text/html", t.text) * "</span>")
 end
-
-macro cz_str(text) return :( Text(text=Markdown.parse($text), lang="cs") ) end
-macro en_str(text) return :( Text(text=Markdown.parse($text), lang="en") ) end
 
 function render_png(s::LaTeXString; dpi=150, debug=false, name=tempname(cleanup=false))
     doc = """
@@ -96,6 +238,26 @@ end
 
 to_html(x) = repr("text/html", x)
 
+# Cloze sub-questions
+function cloze(type::String,
+               answers::String;
+               weight::Int = 1) # Why UInt doesn't work?
+    " {$(weight):$(type):$(answers)} "
+end
+function cloze(type::String, answers::Vector{Choice}; kwargs...)
+    ansstr = join(map(answers) do a
+                  (a.correct ? "=" : "") *
+                  # https://docs.moodle.org/310/en/Embedded_Answers_(Cloze)_question_type#Detailed_syntax_explanations
+                  sprint(escape_chars, join(map(to_html, a.content)), "#~/\"\\")
+                  end, "~")
+    cloze(type, ansstr; kwargs...)
+end
+cloze_answer(answer, feedback) = "$answer#$feedback"
+to_html(yn::YesNo) = cloze("MULTICHOICE", [Choice(yn.yes, ["Yes"]), Choice(!yn.yes, ["No"])])
+to_html(sa::ShortAnswer) = cloze("SA", [Correct(sa.text)])
+to_html(num::Numerical) = cloze("NUMERICAL", "=$(num.val):$(num.tolerance)")
+to_html(mc::MultiChoice) = cloze("MULTICHOICE", "=TODO")
+
 function to_html(x::LaTeXString)
     png = render_png(x)
     io = IOBuffer()
@@ -104,35 +266,7 @@ function to_html(x::LaTeXString)
     """<img src="data:text/png;base64,$(String(take!(io)))">"""
 end
 
-function preview(io::IO, q::Question)
-    println(io, "<h3>Question $(q.id)</h3>")
-    println(io, "<div class=points>Points: $(q.points)</div>")
-    println(io, to_html(q))
-end
-
-function preview(io::IO, cat::Category)
-    println(io, "<h2>Category $(cat.name)</h2>")
-    for q in cat.questions
-        preview(io, q)
-    end
-end
-
-function preview(io::IO, quiz::Quiz)
-    println(io, "<h1>Quiz</h1>")
-    for q in quiz.questions
-        preview(io, q)
-    end
-    for cat in quiz.categories
-        preview(io, cat)
-    end
-end
-
-function preview(object)
-    open("questions.html", "w") do io
-        preview(io, object)
-    end
-    run(`firefox-reload`)
-end
+to_html(a::Vector{Any}) = join(to_html.(a))
 
 function Base.show(io::IO, ::MIME"text/html", q::Question)
     for el in q.content
@@ -147,8 +281,58 @@ function Base.show(io::IO, ::MIME"text/plain", q::Question)
     preview(q)
 end
 
+moodle_xml_type(::TrueFalse) = "truefalse"
+moodle_xml_type(::ShortAnswer) = "shortanswer"
+moodle_xml_type(::Numerical) = "numerical"
+moodle_xml_type(::MultiChoice) = "multichoice"
+moodle_xml_type(::YesNo) = "multichoice"
+moodle_xml_type(::Essay) = "essay"
+moodle_xml_type(x) = nothing
 
-moodle_xml_type(q::Question) = "essay"
+function moodle_xml_type(q::Question)
+    type = moodle_xml_type(q.answer)
+    if isnothing(type) && any(typeof.(q.content) .<: Answer)
+        type = "cloze"
+    end
+   return type
+end
+
+function moodle_xml_answer(xquestion, text::String, fraction::Int; kwargs...)
+    xans = new_child(xquestion, "answer")
+    set_attribute(xans, "fraction", string(fraction))
+    xtext = new_child(xans, "text")
+    add_text(xtext, text)
+    for (tag, val) in kwargs
+        xtag = new_child(xans, tag)
+        add_text(xtag, string(val))
+    end
+end
+
+moodle_xml(tf::TrueFalse, xquestion) = begin
+    moodle_xml_answer(xquestion, "true", tf.correct ? 100 : 0)
+    moodle_xml_answer(xquestion, "false", !tf.correct ? 100 : 0)
+end
+moodle_xml(yn::YesNo, xquestion) = begin
+    add(elem::String, val) = add_text(new_child(xquestion, elem), string(val))
+    add("single", true)
+    moodle_xml_answer(xquestion, "true", yn.yes ? 100 : 0)
+    moodle_xml_answer(xquestion, "false", !yn.yes ? 100 : 0)
+end
+moodle_xml(na::Numerical, xquestion) = moodle_xml_answer(xquestion, na.val, 100, na.tolerance)
+moodle_xml(sa::ShortAnswer, xquestion) = moodle_xml_answer(xquestion, sa.text, sa.fraction)
+# TODO: Multiple short and numerical answers
+moodle_xml(mc::MultiChoice, xquestion) = begin
+    for c in mc.choices
+        moodle_xml_answer(xquestion, to_html(c.content), c.correct ? 100 : 0)
+    end
+
+    add(elem::String, val) = add_text(new_child(xquestion, elem), string(val))
+
+    add("single", mc.single)
+    add("shuffleanswers", mc.shuffle)
+    add("answernumbering", "none")
+end
+moodle_xml(a::Answer, xquestion) = nothing
 
 function moodle_xml(q::Question, xroot)
     xquestion = new_child(xroot, "question")
@@ -163,6 +347,10 @@ function moodle_xml(q::Question, xroot)
     set_attribute(questiontext, "format", "html")
     text = new_child(questiontext, "text")
     add_text(text, repr("text/html", q))
+
+    if q.answer != nothing
+        moodle_xml(q.answer, xquestion)
+    end
 
     add(elem::String, val) = add_text(new_child(xquestion, elem), string(val))
     add(elem::String, val::Bool) = add(elem, Int(val))
@@ -180,7 +368,7 @@ function moodle_xml(cat::Category, xroot)
     # Set the type
     set_attribute(xquestion, "type", "category")
     @pipe xquestion |> new_child(_, "category") |> new_child(_, "text") |> add_text(_, "\$course\$/top/$(cat.name)")
-    for q in cat.questions
+    for q in values(cat.questions)
         moodle_xml(q, xroot)
     end
 end
@@ -196,7 +384,13 @@ function moodle_xml(quiz::Quiz)::XMLDocument
     for q in quiz.questions
         moodle_xml(q, xroot)
     end
+    if default_category in quiz.categories
+        for q in values(default_category.questions)
+            moodle_xml(q, xroot)
+        end
+    end
     for cat in quiz.categories
+        cat === default_category && continue
         moodle_xml(cat, xroot)
     end
     return xdoc
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,83 @@
 using MoodleQuiz
+using MoodleQuiz: moodle_xml_type
 using Test
 
 @testset "MoodleQuiz.jl" begin
-    # Write your tests here.
+
+    q = Question("shortanswer", 1, "", [ "What time is it?" ],
+                 ShortAnswer("13:49"))
+    @test moodle_xml_type(q) == "shortanswer"
+
+    q = Question("essay", 1, "", [ "What did you do today?" ] )
+    @test typeof(q.answer) == Essay
+    @test moodle_xml_type(q) == "essay"
+
+    q = Question("multilang essay", 1, "",
+                 [
+                     en"What time is it?"
+                     cz"Kolik je hodin?"
+                 ])
+
+    q = Question("true/false", 1, "",
+                 [ "Julia is cool." ],
+                 True())
+    @test typeof(q.answer) == TrueFalse
+    @test moodle_xml_type(q) == "truefalse"
+
+    q = Question("yes/no", 1, "",
+                 [ "Is this question nice?" ],
+                 Yes())
+    @test moodle_xml_type(q) == "multichoice"
+
+    q = Question("multichoice", 1, "",
+                 [ "Moodle is (multiple of):" ],
+                 MultiChoice([
+                     Correct("LMS")
+                     Correct("web application")
+                     Wrong("Wiki")
+                     Wrong("desktop application")
+                 ]))
+    moodle_xml(q)
+    @test moodle_xml_type(q) == "multichoice"
+
+    q = Question("singlechoice", 1, "",
+                 [ "Moodle is (single of):" ],
+                 SingleChoice([
+                     Correct("LMS")
+                     Wrong("Wiki")
+                     Wrong("desktop application")
+                 ]))
+    @test moodle_xml_type(q) == "multichoice"
+
+    q = Question("cloze", 1, "",
+                 [
+                    "Moodle is"
+                    ShortAnswer("LMS <&>")
+                      "Its first version was release in year"
+                      Numerical(2002)
+                     "Is this quiz in Moodle?"
+                     Yes()
+                 ])
+    @test moodle_xml_type(q) == "cloze"
+
+    # The escaping rules are documented here:
+    # https://docs.moodle.org/310/en/Embedded_Answers_(Cloze)_question_type#Detailed_syntax_explanations
+    # but either the documentation or our implementation is not
+    # complete, because Moodle fails to import the question if the
+    # characters are in different order :-(
+    q = Question("escaped cloze", 1, "",
+                 [
+                     "Characters that must be escaped in cloze sub-question are (write them space separated)"
+                     ShortAnswer("# ~ \" \\ /")
+                 ])
+    @test occursin("\\# \\~ \\\" \\\\ \\/", MoodleQuiz.to_html(q))
+
+    moodle_xml(q)
+    moodle_xml(MoodleQuiz.default_category)
+    moodle_xml(Quiz())
+
+    if isinteractive()
+        using LightXML
+        save_file(moodle_xml(MoodleQuiz.default_category), "questions.xml")
+    end
 end
